73070d605123e02df942c7fcc1857f1977a5da21
Valentin Aebi <valentin.aebi@epfl.ch>
2022-03-07 12:33:01 +0100

    Document testing system

--- syntactic-whitelist/src/test/scala/TestController.scala
+++ syntactic-whitelist/src/test/scala/TestController.scala
@@ -1,17 +1,35 @@
 import scala.collection.mutable
 
+/**
+ * Controller to ensure that the run method of TestRunner is called
+ *
+ * The purpose of this class is to avoid that some tests pass because the TestRunner was never
+ * instantiated or run() was never called.
+ *
+ * The test class should define a TestController as a class attribute, pass it to each TestRunner that is instantiated
+ * and call assertEmpty when all tests have been executed (e.g. using @AfterClass)
+ */
 class TestController {
   private val allTestIds = mutable.Map[Int, String]()
 
+  /**
+   * Register a TestRunner that must be executed
+   */
   def registerTest(testId: Int, testName: String): Unit = {
     allTestIds.put(testId, testName)
   }
 
+  /**
+   * Unregister a TestRunner
+   */
   def unregisterTest(testId: Int): Unit = {
     val prev = allTestIds.remove(testId)
-    require(prev.isDefined)
+    require(prev.isDefined, "erroneous testId or same test run twice")
   }
 
+  /**
+   * Checks that all registered tests have been unregistered
+   */
   def assertEmpty(): Unit = {
     if (allTestIds.nonEmpty) {
       val faultyTestsNames = allTestIds.values
--- syntactic-whitelist/src/test/scala/TestRunner.scala
+++ syntactic-whitelist/src/test/scala/TestRunner.scala
@@ -3,14 +3,20 @@ import org.junit.Assert.assertEquals
 
 import java.util.concurrent.atomic.AtomicInteger
 
-
+/**
+ * Test runner for whitelist checker tests
+ */
 class TestRunner private(matcher: CheckResult => Unit, filename: String, features: List[Feature],
                          testController: TestController, uid: Int) {
+  import TestRunner._
 
+  /**
+   * Execute the tests
+   */
   def run(): Unit = {
     testController.unregisterTest(uid)
     val checker = Checker(features)
-    val actualRes = checker.checkFile(s"src/test/res/$filename.scala")
+    val actualRes = checker.checkFile(s"$TEST_RES_DIR/$filename.$TEST_FILES_EXT")
     matcher(actualRes)
   }
 
@@ -19,6 +25,9 @@ class TestRunner private(matcher: CheckResult => Unit, filename: String, feature
 object TestRunner {
 
   private val uidGenerator = new AtomicInteger(0)
+
+  private val TEST_RES_DIR = "src/test/res"
+  private val TEST_FILES_EXT = "scala"
 
   class Builder(testController: TestController) {
     private var matcher: Option[CheckResult => Unit] = None
@@ -36,6 +45,9 @@ object TestRunner {
       testController.registerTest(uid, testName)
     }
 
+    /**
+     * @param _filename name of the file to run the checker on (name only, without directory or extension)
+     */
     def onFile(_filename: String): Builder = {
       if (this.filename.isDefined){
         throw new IllegalStateException("filename set more than once")
@@ -44,6 +56,9 @@ object TestRunner {
       this
     }
 
+    /**
+     * @param matcher function that will be called on the resulting CheckResult, in order to check its correctness
+     */
     def expectingMatching(matcher: CheckResult => Unit): Builder = {
       if (this.matcher.isDefined){
         throw new IllegalStateException("matcher set more than once")
@@ -52,6 +67,9 @@ object TestRunner {
       this
     }
 
+    /**
+     * @param expected expected test output, will be compared to the actual one using assertEquals
+     */
     def expectingResult(expected: CheckResult): Builder = {
       if (this.matcher.isDefined){
         throw new IllegalStateException("matcher set more than once")
@@ -62,6 +80,9 @@ object TestRunner {
       this
     }
 
+    /**
+     * @param _features the features that the Checker should allow
+     */
     def withFeatures(_features: Feature*): Builder = {
       if (this.features.isDefined){
         throw new IllegalStateException("features set more than once")
@@ -70,6 +91,9 @@ object TestRunner {
       this
     }
 
+    /**
+     * @return an instance of TestRunner with the provided test execution steps
+     */
     def build(): TestRunner = {
       if (filename.isEmpty) throw new IllegalStateException("cannot build TestRunner: no filename")
       if (matcher.isEmpty) throw new IllegalStateException("cannot build TestRunner: no expectedRes")
