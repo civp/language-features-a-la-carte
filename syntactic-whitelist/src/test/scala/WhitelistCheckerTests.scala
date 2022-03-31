cabd49f530f0b6a45500b6b8a741ebaf238c4b8c
Valentin Aebi <valentin.aebi@epfl.ch>
2022-03-31 20:04:04 +0200

    Go back to createTest(testController) in each test to avoid breaking error reporting

--- syntactic-whitelist/src/test/scala/WhitelistCheckerTests.scala
+++ syntactic-whitelist/src/test/scala/WhitelistCheckerTests.scala
@@ -8,7 +8,7 @@ class WhitelistCheckerTests {
   import WhitelistCheckerTests._
 
   @Test def allowLiteralsAndExpressions_should_allow_literals(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("LiteralsOnly")
       .withFeatures(AllowLiteralsAndExpressions)
       .withDialect(dialects.Sbt1)
@@ -16,7 +16,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def allowLiteralsAndExpressions_should_allow_literals_and_expressions(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("LiteralsAndExpressions")
       .withFeatures(AllowLiteralsAndExpressions)
       .withDialect(dialects.Sbt1)
@@ -24,7 +24,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def vals_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Vals")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowVals)
@@ -32,7 +32,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def vals_should_be_accepted_when_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Vals")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -42,7 +42,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def defs_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Defs")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowDefs)
@@ -50,7 +50,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def allowADTs_should_allow_ADTs(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("ADTs")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -60,7 +60,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def ADTs_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("ADTs")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(
@@ -74,21 +74,21 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowADTs_should_not_allow_sealed_trait_with_non_case_class(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("NotSoADT")
       .withFeatures(AllowADTs)
       .expectInvalid(2 -> 1, 3 -> 1)
   }
 
   @Test def allowBasicOop_should_allow_sealed_trait_with_non_case_class(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("NotSoADT")
       .withFeatures(AllowBasicOop)
       .expectValid()
   }
 
   @Test def allowLiteralFunctions_should_allow_literal_functions(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("LiteralFunctions")
       .withDialect(dialects.Sbt1)
       .withFeatures(
@@ -100,7 +100,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def literal_functions_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("LiteralFunctions")
       .withDialect(dialects.Sbt1)
       .withFeatures(ALL_FEATURES)
@@ -109,7 +109,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def allowForExpr_should_allow_for_expressions(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Fors")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -120,7 +120,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def for_expressions_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Fors")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowForExpr)
@@ -128,14 +128,14 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowImports_should_allow_imports(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("ImportOnly")
       .withFeatures(AllowImports)
       .expectValid()
   }
 
   @Test def imports_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("ImportOnly")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowImports)
@@ -143,14 +143,14 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowPackages_should_allow_packages(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("PackageOnly")
       .withFeatures(AllowPackages)
       .expectValid()
   }
 
   @Test def AllowNull_should_allow_null(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Nulls")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -161,7 +161,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def nulls_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Nulls")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowNull)
@@ -169,7 +169,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowPolymorphicTypes_should_allow_polymorphic_types(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Polymorphism")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -181,7 +181,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def polymorphic_types_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Polymorphism")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowPolymorphicTypes)
@@ -189,7 +189,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowLaziness_should_allow_lazy_vals_and_args(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Laziness")
       .withFeatures(
         AllowVals,
@@ -201,7 +201,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def laziness_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Laziness")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowLaziness)
@@ -209,7 +209,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowAdvancedOop_should_allow_advanced_oop_constructs(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("AdvancedOOP")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -220,7 +220,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def advanced_oop_constructs_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("AdvancedOOP")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowAdvancedOop)
@@ -228,7 +228,7 @@ class WhitelistCheckerTests {
   }
   
   @Test def AllowImperativeConstructs_should_allow_imperative_constructs(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Imperative")
       .withDialect(dialects.Sbt1)
       .withFeatures(
@@ -240,7 +240,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def imperative_constructs_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Imperative")
       .withDialect(dialects.Sbt1)
       .withFeatures(ALL_FEATURES)
@@ -249,7 +249,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowContextualConstructs_should_allow_contextual_constructs(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Contextual")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -261,7 +261,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def contextual_constructs_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Contextual")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowContextualConstructs)
@@ -269,7 +269,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowExtensions_should_allow_extensions(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Extensions")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -280,7 +280,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def extensions_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Extensions")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowExtensions)
@@ -288,7 +288,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowMetaprogramming_should_allow_macros(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Metaprogramming")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -301,7 +301,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def metaprogramming_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Metaprogramming")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowMetaprogramming)
@@ -309,7 +309,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowExports_should_allow_exports(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Export")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -321,7 +321,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def exports_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Export")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowExports)
@@ -329,7 +329,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowXml_should_allow_xml(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Xml")
       .withFeatures(
         AllowVals,
@@ -339,7 +339,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def xml_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Xml")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowXml)
@@ -347,7 +347,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowStringInterpolation_should_allow_string_interpolation(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("StringInterpolation")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -358,7 +358,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def string_interpolation_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("StringInterpolation")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowStringInterpolation)
@@ -366,7 +366,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowAnnotations_should_allow_annotations(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Annotations")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -378,7 +378,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def annotations_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Annotations")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowAnnotations)
@@ -386,7 +386,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowInfixes_should_allow_infixes(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Infixes")
       .withFeatures(
         AllowDefs,
@@ -397,7 +397,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def infixes_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Infixes")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowInfixes)
@@ -405,7 +405,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def AllowInlines_should_allow_inlines(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Inlines")
       .withFeatures(
         AllowDefs,
@@ -416,7 +416,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def inlines_should_be_rejected_when_not_allowed(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("Inlines")
       .withFeatures(ALL_FEATURES)
       .exceptFeatures(AllowInlines)
@@ -424,7 +424,7 @@ class WhitelistCheckerTests {
   }
 
   @Test def checker_should_support_classes_without_braces(): Unit = {
-    newTest
+    createTest(testController)
       .onFile("NoBraces")
       .withFeatures(
         AllowLiteralsAndExpressions,
@@ -438,8 +438,6 @@ class WhitelistCheckerTests {
 
 object WhitelistCheckerTests {
   val testController = new TestController()
-
-  private def newTest = createTest(testController)
 
   @AfterClass
   def checkAllRun(): Unit = {
