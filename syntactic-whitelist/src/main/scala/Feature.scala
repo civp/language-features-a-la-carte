c5833de06f58e8fc9da94d46a2885844d9007de3
Valentin Aebi <valentin.aebi@epfl.ch>
2022-04-07 16:03:10 +0200

    Structural equality in CompositeFeature

--- syntactic-whitelist/src/main/scala/Feature.scala
+++ syntactic-whitelist/src/main/scala/Feature.scala
@@ -7,6 +7,7 @@ trait Feature {
 
   /**
    * Checks whether a tree is allowed by this feature
+   *
    * @param tree the tree to check
    * @return true iff it is allowed
    */
@@ -19,15 +20,27 @@ object Feature {
   /**
    * Feature defined as a combination of other features
    * A composite feature allows a tree iff at least one of the underlying features accepts it
+   *
    * @param features the features to allow
    */
-  class CompositeFeature(features: List[Feature]) extends Feature {
-    def this(features: Feature*) = this(features.toList)
+  class CompositeFeature(private val features: Set[Feature]) extends Feature {
+    def this(features: List[Feature]) = this(features.toSet)
+    def this(features: Feature*) = this(features.toSet)
+
     override def allows(tree: Tree): Boolean = features.exists(_.allows(tree))
+
+    override def equals(other: Any): Boolean = other match {
+      case that: CompositeFeature =>
+        features == that.features
+      case _ => false
+    }
+
+    override def hashCode(): Int = features.hashCode()
   }
 
   /**
    * Adapter to create a feature given the partialfunction describing it
+   *
    * @param checkPF this function should return true when given a tree that is allowed and not be defined on other trees
    *                (or return false on them, but this is not necessary)
    */
