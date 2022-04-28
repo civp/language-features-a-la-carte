c1496a58d313a40dbd25a95501327bced28f7706
Valentin Aebi <valentin.aebi@epfl.ch>
2022-04-28 20:18:19 +0200

    Methods to check a program given as BufferedSource or File

--- syntactic/src/main/scala/syntactic_checker/Checker.scala
+++ syntactic/src/main/scala/syntactic_checker/Checker.scala
@@ -1,5 +1,7 @@
 package syntactic_checker
 
+import java.io.File
+import scala.io.BufferedSource
 import scala.meta.{Dialect, Source, Tree}
 import scala.util.control.NonFatal
 import scala.util.{Failure, Success, Try, Using}
@@ -47,6 +49,34 @@ trait Checker {
   }
 
   /**
+   * Check whether the program contained in the given source uses only allowed features
+   *
+   * @param dialect the Scala dialect that the parsing should use
+   * @param bufferedSource the source of the program to be checked
+   * @return a CheckResult (Valid, Invalid or ParsingError)
+   */
+  final def checkBufferedSource(dialect: Dialect, bufferedSource: BufferedSource): CheckResult = {
+    val content = Using(bufferedSource) { bufferedSource =>
+      bufferedSource.getLines().mkString("\n")
+    }
+    content.map(checkCodeString(dialect, _)) match {
+      case Failure(exception) => CheckResult.ParsingError(exception)
+      case Success(checkRes) => checkRes
+    }
+  }
+
+  /**
+   * Check whether the program contained in the given file uses only allowed features
+   *
+   * @param dialect  the Scala dialect that the parsing should use
+   * @param file the file containing the program to be checked
+   * @return a CheckResult (Valid, Invalid or ParsingError)
+   */
+  final def checkFile(dialect: Dialect, file: File): CheckResult = {
+    checkBufferedSource(dialect, scala.io.Source.fromFile(file))
+  }
+
+  /**
    * Check whether the program contained in the file with the given filename uses only allowed features
    *
    * @param dialect  the Scala dialect that the parsing should use
@@ -54,13 +84,7 @@ trait Checker {
    * @return a CheckResult (Valid, Invalid or ParsingError)
    */
   final def checkFile(dialect: Dialect, filename: String): CheckResult = {
-    val content = Using(scala.io.Source.fromFile(filename)) { bufferedSource =>
-      bufferedSource.getLines().mkString("\n")
-    }
-    content.map(checkCodeString(dialect, _)) match {
-      case Failure(exception) => CheckResult.ParsingError(exception)
-      case Success(checkRes) => checkRes
-    }
+    checkBufferedSource(dialect, scala.io.Source.fromFile(filename))
   }
 
 }
