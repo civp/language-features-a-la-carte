b490a63e6d5541b107596f55720ec9833175d3ff
Valentin Aebi <valentin.aebi@epfl.ch>
2022-06-03 14:26:34 +0200

    findValsToInline

--- functional-converter/src/main/scala/translator/NamesFinder.scala
+++ functional-converter/src/main/scala/translator/NamesFinder.scala
@@ -1,5 +1,7 @@
 package translator
 
+import scala.meta.Term.Block
+import scala.meta.transversers.Traverser
 import scala.meta.{Defn, Pat, Stat, Term, Tree}
 
 object NamesFinder {
@@ -15,14 +17,14 @@ object NamesFinder {
   }
 
   def allUpdatedVars(stats: List[Stat]): Set[String] = {
-    allNodesMatching(stats){
+    allNodesMatching(stats) {
       case Term.Assign(Term.Name(nameStr), _) => nameStr
       case assign@Term.Assign(_, _) => throw UnexpectedConstructException(assign)
     }.toSet
   }
 
   def allDeclaredVars(stats: List[Stat]): Set[String] = {
-    allNodesMatching(stats){
+    allNodesMatching(stats) {
       case varDef@Defn.Var(mods, pats, optType, optTerm) if pats.size > 1 =>
         throw TranslaterException(s"not supported: $varDef")
       case Defn.Var(mods, List(Pat.Var(Term.Name(nameStr))), optType, optTerm) => nameStr
@@ -31,13 +33,34 @@ object NamesFinder {
   }
 
   def allReferencedVars(stats: List[Stat]): Set[String] = {
-    allNodesMatching(stats){ case Term.Name(nameStr) => nameStr }.toSet
+    allNodesMatching(stats) { case Term.Name(nameStr) => nameStr }.toSet
   }
 
-  def allDeclaredVals(stats: List[Stat]): Set[List[Pat]] = {
-    allNodesMatching(stats){
-      case Defn.Val(mods, pats, optType, optTerm) => pats
-    }.toSet   // TODO implement this method
+  // TODO test
+  def findValsToInline(stats: List[Stat]): Set[String] = {
+    val declared = Set.newBuilder[Term.Name]
+    val referenced = List.newBuilder[Term.Name]
+    stats.foreach(stat =>
+      new Traverser {
+        override def apply(tree: Tree): Unit = tree match {
+          case _: Block => /* ignore children */
+          case valDefn@Defn.Val(_, List(Pat.Var(name)), _, _) => {
+            declared += name
+            super.apply(valDefn)
+          }
+          case name: Term.Name => {
+            referenced += name
+            super.apply(name)
+          }
+        }
+      }.apply(stat)
+    )
+    val referencedOnce = referenced.result()
+      .filter(!declared.result().contains(_))
+      .groupBy(identity)
+      .filter(_._2.size == 1)
+      .keys
+    referencedOnce.map(_.value).toSet
   }
 
 }
