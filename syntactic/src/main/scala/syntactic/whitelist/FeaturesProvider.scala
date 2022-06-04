879bc88a2af874cff23b5e604596e61de6b9b2b9
Valentin Aebi <valentin.aebi@epfl.ch>
2022-06-05 00:53:28 +0200

    Make comment clearer

--- syntactic/src/main/scala/syntactic/whitelist/FeaturesProvider.scala
+++ syntactic/src/main/scala/syntactic/whitelist/FeaturesProvider.scala
@@ -21,10 +21,14 @@ trait FeaturesProvider {
       .members
       .filter(_.isPublic)
       .filter(_.isModule)
-      // TODO either one or the other of the two lines (and if relying on type then change the exception in the PF below)
+
+      // TODO either one or the other of both lines
+      // first line relies on an annotation
+      // second line includes all public Features in the list (would require to change the exception type in the PF below)
       .filter(_.annotations.exists(_.tree.tpe =:= mirror.typeOf[ScalaFeature]))
       //.filter(_.asModule.typeSignature <:< mirror.typeOf[Feature])
       .map(member => mirror.reflectModule(member.asModule).instance)
+
     val modules = modulesWithUncheckedType.map {
       case feature: Feature => feature
       case any => throw new AnnotationTypeMismatchException(null, any.getClass.getName)
