723047db6fd4c29e49fab29f9ad64f145f3572a7
civp <chiefpan0@gmail.com>
2022-06-05 19:59:15 +0200

    Correct test input headers

--- tests/unit/src/test/scala/SyntacticBlacklistSuite.scala
+++ tests/unit/src/test/scala/SyntacticBlacklistSuite.scala
@@ -8,13 +8,13 @@ import scala.meta.dialects.Scala213
 class SyntacticBlacklistSuite extends SyntacticSuite {
   
   test("no-null-no-cast") {
-    val path = getTestPath("blacklist/NoNullNoCast")
+    val path = getTestPath("blacklist/NoNullCast")
     val checker = BlacklistChecker(NoNull, NoCast)
     checkPath(checker, path, Scala213)
   }
 
   test("no-var-no-while") {
-    val path = getTestPath("blacklist/NoVarNoWhile")
+    val path = getTestPath("blacklist/NoVarWhile")
     val checker = BlacklistChecker(NoVar, NoWhile)
     checkPath(checker, path, Scala213)
   }
